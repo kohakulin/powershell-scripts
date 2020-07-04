f7fdf6827c0c808185534e0b7c2f25f05a45a1af
Ting Yen Lin <kohaku.lin@gmail.com>
2020-07-04 12:59:53 -0300

    change: script

--- task 1/files-afterscript/script .ps1
+++ task 1/files-afterscript/script .ps1
@@ -1,4 +1,7 @@
-dir | Rename-Item -NewName {$_.name -replace "mydocument","Task Done"}
-dir | Rename-Item -NewName {$_.name -replace " - copy*"," "}
-dir | Rename-Item -NewName {$_.basename + "-ok" + $_.extension}
+dir | Rename-Item -NewName {$_.name -replace "mydocument","Task-Done_1"}
+dir | Rename-Item -NewName {$_.name -replace " - Copy ",".2."}
+dir | Rename-Item -NewName {$_.name -replace " - Copy",".2.9"}
+dir | Rename-Item -NewName {$_.name -replace "_1.2.","_"}
+dir | Rename-Item -NewName {$_.name -replace "\(",""}
+dir | Rename-Item -NewName {$_.name -replace "\)",""}
 
